3201c9ac964b3fe2c4717d4567b3d0f1b8ed94d0
DeppMeng <depp_meng@bitbucket.org>
2018-10-17 16:13:21 +0800

    modified

--- run.sh
+++ run.sh
@@ -1,17 +1,42 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Set paths
 INCEPTION_V1_PRETRAINED=pretrained/bvlc_googlenet.caffemodel.pth
 CPM_PRETRAINED=pretrained/pose_iter_440000.caffemodel.pth
 
-MARKET1501_DATA_ROOT=../market
+MARKET1501_DATA_ROOT="/userhome/reid/part_bilinear_reid/data/market1501/raw/Market-1501-v15.09.15"
 
 export MARKET1501_DATA_ROOT=$MARKET1501_DATA_ROOT
 export MARKET1501_TRAIN_LIST=$MARKET1501_TRAIN_LIST
 export INCEPTION_V1_PRETRAINED=$INCEPTION_V1_PRETRAINED
 export CPM_PRETRAINED=$CPM_PRETRAINED
 
-CONFIGURE_PATH="./configs/config.sh"
+PYTHON='python'
+GPU_ID="0,1"
+
+# model
+ARCH='inception_v1_cpm'
+FEATURES=512
+DILATION=2
+USE_RELU=false
+
+# data
+DATASET='market1501'
+HEIGHT=160
+WIDTH=80
+CROP_HEIGHT=160
+CROP_WIDTH=80
+BATCH_SIZE=250
+USE_CAFFE_SAMPLER=true # [true | false]
+
+# optimizer
+OPTIMIZER='sgd_caffe'
+LR=0.01
+WEIGHT_DECAY=0.0002
+EPOCHS=750
+
+LOG_DIR="logs/$DATASET/$EXP_ID"
+
 
 # Download pretrained network weights
 if ! [ -d pretrained ]; then
